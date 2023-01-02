483d6c3f8caa44902d3824fa41f70a9561a7e1f6
Yash Kalariya <yash.k@ixfi.com>
2023-01-02 16:49:16 +0530

    flutter chart border issue fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3feb1884dbf51b74244d2854c785bffa",
-"index.html": "d9da03933c1d4a8d5dab01d6e74495c9",
-"/": "d9da03933c1d4a8d5dab01d6e74495c9",
-"main.dart.js": "f7c041928495196a81c0e90577b24b31",
+"index.html": "bad60a42d5cc624e81c36796178dca4a",
+"/": "bad60a42d5cc624e81c36796178dca4a",
+"main.dart.js": "f5476cc2272708129e8dc45d150ccf26",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
