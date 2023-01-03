9aa514a67ad7de6ea30cea1b09151dbd3ced7f59
Yash Kalariya <yash.k@ixfi.com>
2023-01-03 11:37:21 +0530

    zoom actions

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "3feb1884dbf51b74244d2854c785bffa",
-"index.html": "61f9b0df68ad1f2a4b571819a214076f",
-"/": "61f9b0df68ad1f2a4b571819a214076f",
-"main.dart.js": "0ce287ca200a371bd0acc834949f597e",
+"index.html": "6b8afd179d41befdf7f77d0fc219ba37",
+"/": "6b8afd179d41befdf7f77d0fc219ba37",
+"main.dart.js": "9c85ef26ffe03026ddaf33ad783611a9",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
